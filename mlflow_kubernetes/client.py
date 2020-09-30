7e4b6b5c1e302854cb350c05c00dcbf9fef5cc88
logan <chong.luo@cidatahub.com>
2020-09-30 18:35:41 +0800

    chore(client): use node host directly, handle exception more precisely

--- mlflow_kubernetes/client.py
+++ mlflow_kubernetes/client.py
@@ -1,6 +1,8 @@
 """
 invoke mlflow deployed in kubernetes clusts through construct a ``ModelService`` instance.
 """
+from itertools import product
+
 import pandas
 import requests
 from kubernetes import config as kube_config, client
@@ -20,17 +22,35 @@ class ModelService:
     kubernetes service client provide inference from input dataframe
     """
 
-    def __init__(self, model_name, kube_config_path=None):
+    def __init__(self, model_name, version, kube_config_path=None):
         kube_config.load_kube_config(config_file=kube_config_path)
-        kube_cluster_info = client.Configuration()
         self._request = requests.Session()
-        self._request.headers.update("Authorization", f"Bearer {config.KUBE_AUTH_TOKEN}")
-        self.model_name = model_name
-        # TODO(logan): use nodeport type in production. docker-kube not expose port in cluster
-        self._kube_service_proxiy_uri = f'{kube_cluster_info.host}/api/v1/namespaces/{config.KUBE_MLLFOW_MODELS_NAMESPACE}' \
-                                        f'/services/{model_name}/proxy/invocations'
+        self.model_name = f"{model_name}-{version}"
+        self._kube = client.CoreV1Api()
+        # host port used to access model service running in kubernetes
+        self._host_port_pairs = set()
 
-    def predict(self, df):
+    def get_service_host_port(self):
+        name = self.model_name
+        app = self._kube
+        hosts = [
+            container.status.host_ip
+            for container in app.list_namespaced_pod(
+                namespace=config.KUBE_MLLFOW_MODELS_NAMESPACE, label_selector=f"name={name}"
+            ).items
+            if container.status.phase == 'Running'
+        ]
+
+        ports = [
+            port.node_port
+            for port in app.read_namespaced_service(
+                name=name, namespace=config.KUBE_MLLFOW_MODELS_NAMESPACE
+            ).spec.ports
+        ]
+
+        return set(product(hosts, ports))
+
+    def predict(self, df: pandas.DataFrame) -> pandas.DataFrame:
         """
         method input/output interface like :py:meth:`mlflow.pyfunc.PyFuncModel.predict
         :param df: dict of dataframe
@@ -40,7 +60,30 @@ class ModelService:
             body = _df_to_dict(df)
         else:
             body = df
-        resp = self._request.post(self._kube_service_proxiy_uri, json=body)
-        if not resp.status_code == 200:
-            resp.raise_for_status()
-        return resp.json()
+
+        if not self._host_port_pairs:
+            self._host_port_pairs = self.get_service_host_port()
+
+        invalidate_idx = set()
+        resp = None
+        for host, port in self._host_port_pairs:
+            try:
+                resp = self._request.post(f'http://{host}:{port}/invocations', json=body)
+            except requests.exceptions.ConnectionError:
+                invalidate_idx.add((host, port))
+                continue
+            else:
+                break
+
+        self._host_port_pairs -= invalidate_idx
+        # none of these request success connected
+        if resp is None:
+            raise ConnectionError(
+                'connection failed to all services: {}'.format(','.join(
+                    ['%s:%s' % (host, port) for host, port in invalidate_idx]
+                )))
+
+        if resp.status_code != 200:
+            raise ValueError(resp.json())
+
+        return pandas.DataFrame(resp.json())
